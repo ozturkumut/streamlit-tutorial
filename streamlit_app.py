a45dadf8565bfc6b31a0ba5fb0ba5218f5ecbbdc
ozturkumut <umut@deventral.com>
2023-06-04 12:15:33 +0100

    title changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain.llms import OpenAI
 
-st.title('🦜🔗 Quickstart App')
+st.title('🦜🔗 Quickstart Application')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
